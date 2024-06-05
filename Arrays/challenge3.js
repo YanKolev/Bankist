a6b08de8db58214f7448b0c078b2de607b5534de
Yan Kolev <y.kolev11@gmail.com>
2024-06-05 18:55:05 +0300

    adding challenge3

---
+++ Arrays/challenge3.js
@@ -0,0 +1,20 @@
+/*
+
+Coding Challenge #3
+Rewrite the 'calcAverageHumanAge' function from Challenge #2, but this time
+as an arrow function, and using chaining!
+Test data:
+§Data 1: [5, 2, 4, 1, 15, 8, 3]
+§Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+--------------------------------------------------
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is
+<= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old,
+humanAge = 16 + dogAge * 4
+2. Exclude all dogs that are less than 18 human years old (which is the same as
+keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know
+from other challenges how we calculate averages 😉)
+*/
