56d5ef45feb39ed1c7b8f622a91c71e04c064311
Yan Kolev <y.kolev11@gmail.com>
2024-06-07 13:35:46 +0300

    adding user functionality

--- Arrays/script.js
+++ Arrays/script.js
@@ -87,7 +87,7 @@ const displayMovements = function(movements) {
 };
 
 
-displayMovements(account1.movements)
+
 
 //display balance function with movements and reduce method.
 const calcDisplayBalance = function(movements){
@@ -95,24 +95,24 @@ const calcDisplayBalance = function(movements){
   labelBalance.textContent = `${balance} EUR`
 };
 
-calcDisplayBalance(account1.movements);
+
 
 //creating  display summary function (incomes, outcomes, interest)
 
-const calcDisplaySummary = function (movements){
-  const incomes = movements
+const calcDisplaySummary = function (acc){
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}E`
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov <0)
     .reduce((acc, mov) => acc +mov, 0)
   labelSumOut.textContent = `${Math.abs(out)}E`
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov >0)
-    .map(deposit => deposit *1,2/100)
+    .map(deposit => deposit *acc.interestRate/100)
     .filter((int, i , arr) =>{ //in case there is a new rule if the bank decideds to exclude interest lower than 1 
       console.log(arr);
       return int >=1;
@@ -150,6 +150,45 @@ const createUsernames = function(accs){
 createUsernames(accounts);
 console.log(accounts);
 
+//Event handlers
+
+let currentAccount; 
+
+
+btnLogin.addEventListener('click', function(e){
+  //prevent form from submitting
+  e.preventDefault()
+  console.log('LOGIN')
+
+  //in order to log the user, we need to find the account from the accounts array 
+  // username's from the user that input it
+  currentAccount = accounts.find(acc =>acc.username === inputLoginUsername.value);
+  console.log(currentAccount);
+
+  if(currentAccount?.pin === Number(inputLoginPin.value)){
+    
+    //Display UI and message
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`;
+
+    containerApp.style.opacity = 100;
+
+    //Clear the input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    //Display movements
+    displayMovements(currentAccount.movements);
+
+    //Display balance
+    calcDisplayBalance(currentAccount.movements);
+
+    //Display summary
+    calcDisplaySummary(currentAccount);
+    
+    
+  }
+
+});
 
 
 
@@ -477,3 +516,5 @@ console.log(accounts);
 const account = accounts.find(acc => acc.owner === 'Jesica Davis');
  //when we loop over the accounts, each account is an element
 console.log(account);
+
+
