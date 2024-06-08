52fbe6134bdb5901661e36d024b84dc9a8c36e1d
Yan Kolev <y.kolev11@gmail.com>
2024-06-08 15:44:25 +0300

    ES6 findIndex/acc deletion functionality

--- Arrays/script.js
+++ Arrays/script.js
@@ -227,7 +227,41 @@ btnTransfer.addEventListener('click', function(e){
     updateUI(currentAccount);
 
   }
+});
+
+
+//Findindex method- works same as find, just returns the index instead of the element.
+//closing account feature-> means to delete that acc object from the accounts array. 
+//to delete an element from the array we use a splice method 
+
+
+//First we need to check if the credentials are correct and then proceed with the deletion 
+
+btnClose.addEventListener('click', function(e){
+  e.preventDefault();
+
+  
+  
+  if(inputCloseUsername.value === currentAccount.username && (inputClosePin.value) === currentAccount.pin){
+
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
+
+    accounts.splice(index, 1);//splice it will mutate the array and delete
+
+    //in order to "delete the account we will need to log it out and hide the UI"
+
+    containerApp.style.opacity = 0;
+
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
 })
+
+//both find and findIndex method get access to current index and the current entire array.
+// both were addad in ES6, so there will not work in old browsers.
+
+
+
 
 
 /////////////////////////////////////////////////
