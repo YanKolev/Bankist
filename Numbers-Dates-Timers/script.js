37b2540796edc323e4bf977f25df3a4ef69b34eb
Yan Kolev <y.kolev11@gmail.com>
2024-06-21 14:00:50 +0200

    Math Rounding, fixing display bugs

--- Numbers-Dates-Timers/script.js
+++ Numbers-Dates-Timers/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -299,3 +299,65 @@ console.log(Number.isFinite(23 / 0)) // False
 console.log(Number.isInteger(23))
 console.log(Number.isInteger(23.0))
 console.log(Number.isInteger(23 / 0))
+
+
+
+
+// --- Math and Rounding ---
+
+//square root
+console.log(Math.sqrt(25)); //5
+console.log(25 **(1/2)); //5
+console.log(8 **(1 / 3)); //2
+
+console.log(Math.max(5,18, 23, 11, 2)) //returns 23- as it returs the maximum number
+//.max- makes type coercion and it does not parsing- 
+console.log(Math.max(5,18, '23', 11, 2)) //string 23 will be type coerced and outcome is 23
+console.log(Math.max(5,18, '23px', 11, 2)) //it will not PARSE IT, result will be NaN
+
+//as we have max, we have min also: 
+console.log(Math.min(5,18, 23, 11, 2)) 
+
+
+//if you want to calculate the area of a circle with radius of 10px
+console.log(Math.PI * Number.parseFloat('10px') ** 2)
+
+//random generation with the math method
+console.log(Math.trunc(Math.random() * 6)+1);
+
+//formula to generate random integers between 2 values: 
+const randomInt = (min, max) => Math.trunc(Math.random() * (min-max) +1) + min;
+// 0 .....1 -> 0 ... (max -min) -> min...max
+console.log(randomInt(10,20));
+
+//rounding integers
+console.log(Math.trunc(23.3));
+
+//- round- it will round to the nearest integer
+console.log(Math.round(23.3)) // result will be 23
+console.log(Math.round(23.9)) // result will be 24
+
+//-ceil- Always rounding up
+console.log(Math.ceil(23.3)); //24 
+console.log(Math.ceil(23.9)); //24
+
+//-floor- Always rounding down
+console.log(Math.floor(23.3)); //23
+console.log(Math.floor(23.9)); //23
+
+//All of the types do type coercion so even if its a string it will convert it to a integer
+
+//However if you compare trunc and floor, while using a negative number, results will be different
+console.log(Math.trunc(-23.3)); //trunc will cut off at -23
+console.log(Math.floor(-23.3)); //floor will round down to -24, as rounding works the other way around with negative numbers
+
+//floor works a bit better than trunc, as it can be used in all situation, even with negative outputs
+const randomIntv2 = (min, max) => Math.floor(Math.random() * (min-max) + 1) + min;
+
+//rounding decimal point numbers
+
+console.log((2.7).toFixed(0)) //it will convert to string 3
+console.log((2.7).toFixed(3))
+console.log((2.345).toFixed(2)) //string 2.35
+console.log(+(2.345).toFixed(2)) // number 2.35
+
