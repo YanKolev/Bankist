5ddb167dcc1eac450b09ca6c8387d3676eeba693
Yan Kolev <y.kolev11@gmail.com>
2024-06-30 17:45:55 +0200

    tabbed components

--- Advanced-dom/script.js
+++ Advanced-dom/script.js
@@ -71,7 +71,36 @@ document.querySelector('.nav__links').addEventListener('click', function (e){
 })
 
 
+//Tabbed component
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+//we will need to use event delegation we need to attach the event handler to the common parent element. 
+
+tabsContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.operations__tab')
+
+
+
+
+   // Guard clause
+  if(!clicked) return
+  
+
+  //Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList. remove('operations__content--active'));
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+  .classList.add('operations__content--active')
+});
 
 
 
@@ -438,3 +467,12 @@ console.log(h1.parentElement.children)
 })
 
 */
+
+
+//----Building tabbed components----
+
+//tabbed component- has some tabs, and the content of the area below changes-> like browser tab
+
+//htmp div class operations, we will not create a new tab, we will hide the already existent info
+
+
