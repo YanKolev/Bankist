77702db295ef3363b8f5b5fb469a32974839bfdf
Yan Kolev <y.kolev11@gmail.com>
2024-07-01 18:56:50 +0200

    Reveal sections with Intersection Observer API

--- Advanced-dom/script.js
+++ Advanced-dom/script.js
@@ -196,6 +196,34 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 headerObserver.observe(header);
 
 
+// Revealing images on scroll with no external library 
+//using the intersection observer API 
+
+//Reveal section 
+
+const allSections = document.querySelectorAll('.section')
+
+const revealSection = function(entries, observer){
+ const [entry] = entries;
+
+ if(!entry.isIntersecting) return;
+
+ entry.target.classList.remove('section--hidden');
+ observer.unobserve(entry.target);
+
+}
+
+const sectionObserver = new IntersectionObserver
+(revealSection, {
+  root:null,
+  threshold: 0.15 //15% visible
+})
+
+
+allSections.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+})
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
