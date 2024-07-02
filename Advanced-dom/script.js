cbdc4aea82e7e1d32db5bcead252720bbadfc9d5
Yan Kolev <y.kolev11@gmail.com>
2024-07-02 14:08:25 +0200

    slider part1

--- Advanced-dom/script.js
+++ Advanced-dom/script.js
@@ -255,6 +255,67 @@ const imgObserver = new IntersectionObserver(loading, {
 imgTargets.forEach(img => imgObserver.observe(img))
 
 
+//SLIDER COMPONENTS 
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft= document.querySelector('.slider__btn--left');
+const btnRight= document.querySelector('.slider__btn--right');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+/*
+const silder = document.querySelector('.slider');
+silder.style.transform = 'scale(0.5)';
+silder.style.overflow = 'visible'; 
+*/
+
+
+
+const goToSlide = function(slide){
+  slides.forEach((s,i) => (s.style.transform =
+    `transalateX(${100 * (i-curSlide)}%)`))
+}
+
+goToSlide(0)
+
+//Next slide - change the percentages, so the slide we want to go is 0%
+
+const nextSlide = function(){
+  if (curSlide ===maxSlide -1){
+    curSlide = 0;
+  } else {
+    curSlide++;
+
+  }
+
+  goToSlide(curSlide)
+  }
+
+const prevSlide = function(){
+  if(curSlide===0){
+    curSlide =maxSlide-1;
+  } else {
+    curSlide--;
+  }
+
+  
+  goToSlide(curSlide);
+};
+
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+  
+
+  
+
+
+
+
+
+// curSlite= 1, -100%, 0%, 100%, 200%
+
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //                           Course notes:
